da3daa6c5a38f89aad721c2abe4d8c9325269d74
masanori-komehana <k.comet725@gmail.com>
2021-10-12 16:04:54 +0900

    Update run.sh

--- document/run.sh
+++ document/run.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+make clean
 make html
 
-firefox build/html/index.html
+read -p "Open HTML File? (y/N): " yn
+case "$yn" in
+  [yY]*) 
+      echo open build/html/index.html via Firefox...
+      firefox build/html/index.html > /dev/null 2>&1 &
+      ;;
+  *) ;;
+esac
 
-
+echo build process end.
