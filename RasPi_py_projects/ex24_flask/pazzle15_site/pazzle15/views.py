80625f88efd59ee2396b796ef4a3ca9361bc6d21
masanori-komehana <k.comet725@gmail.com>
2021-12-21 00:37:29 +0900

    ページ分けと統計用のクエリ発行

--- RasPi_py_projects/ex24_flask/pazzle15_site/pazzle15/views.py
+++ RasPi_py_projects/ex24_flask/pazzle15_site/pazzle15/views.py
@@ -1,6 +1,9 @@
+from pprint import pprint
+
 from flask import request, redirect, url_for, render_template, flash
 from pazzle15 import app, db
 from pazzle15.models import Player, Result
+
 
 @app.route('/')
 def show_entries():
@@ -28,6 +31,7 @@ def results2():
 def players():
     players = Player.query.all()
     return render_template('players.html', players=players)
+    
 @app.route('/players', methods=['POST'])
 def select_player():
     select_id = int(request.form['select_id'])
@@ -42,6 +46,45 @@ def select_player():
     print(f"player change: {current_id} -> {select_id}")
     flash(f'Player Selected: {player2.name}')
     return redirect(url_for('players'))
+
+@app.route('/statistics')
+def statistics():
+    result = db.engine.execute("select" +
+        "    player.player_id as 'player_id'," +
+        "    player.name as 'name'," +
+        "    count(*) as 'play_count'," +
+        "    (sum(result.pazzletime)) as 'played_time'," +
+        "    (min(result.pazzletime)) as 'best_time'," +
+        "    (max(result.pazzletime)) as 'worst_time'," +
+        "    (avg(result.pazzletime)) as 'avg_time'," +
+        "    min(result.movecount) as 'best_movecount'," +
+        "    max(result.movecount) as 'worst_movecount'," +
+        "    avg(result.movecount) as 'avg_movecount' " +
+        "from" +
+        "    result " +
+        "inner join " +
+        "    player " +
+        "on" +
+        "    result.player_id=player.player_id " +
+        "group by" +
+        "    player.name " +
+        "order by " +
+        " player.player_id asc " +
+        ";")
+    pprint([dict(row) for row in result])
+    return "This is Statistics Page. Coming Soon..."
+
+@app.route('/paged_results')
+def paged_results_0():
+    return redirect(url_for('paged_results', page=1))
+
+@app.route('/paged_results/<int:page>', methods=['GET'])
+def paged_results(page=1):
+    per_page = 10
+    results = Result.query.order_by(Result.id.asc()).paginate(
+        page, per_page, error_out=False)
+    page_max = (results.total // per_page)
+    return render_template('paged_results.html', page_max, per_page=per_page, paged_results=results, page=page)
 
 @app.route('/add', methods=['POST'])
 def add_entry():
